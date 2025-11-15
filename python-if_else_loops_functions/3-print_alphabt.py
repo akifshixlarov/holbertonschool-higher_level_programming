999999406ee0c8e6865d7241e4415f59b5badc4f
Akif <akifshixlarov@gmail.com>
2025-11-15 22:12:19 +0400

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-print(
-    "".join("{:c}".format(i) for i in range(97, 123) if i != 101 and i != 113),
-    end=""
-)
+alphabet = ""
+for i in range(97, 123):
+    if chr(i) != "q" and chr(i) != "e":
+        alphabet += chr(i)
+print("{}".format(alphabet), end="")
