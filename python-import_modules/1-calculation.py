2b639588f768a0d9f4d4e571ded2331e7f241449
Akif <akifshixlarov@gmail.com>
2025-11-18 15:00:09 +0400

    1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    args = argv[1:]
+    n = len(args)
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+    for i, a in enumerate(args, 1):
+        print("{}: {}".format(i, a))
