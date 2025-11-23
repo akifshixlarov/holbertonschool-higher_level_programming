94de70e803a1f4be66ab5f9a7400c372dcbb080e
Akif <akifshixlarov@gmail.com>
2025-11-23 15:33:08 +0400

    task6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers.
+
+    Args:
+        matrix (list of lists): The matrix to print.
+    """
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
