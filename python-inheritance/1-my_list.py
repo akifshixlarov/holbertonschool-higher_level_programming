1e9133f5554b43c4ec6d1f552f0b43f012799cdf
Akif <akifshixlarov@gmail.com>
2025-11-28 16:02:54 +0400

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ class MyList that inherits from list"""
 
+
 class MyList(list):
     """A class that inherits from list and adds a print_sorted method"""
 
+    
     def print_sorted(self):
-        """Prints the list sorted in ascending order without modifying the original list"""
+        """Prints the list sorted in ascending order"""
         print(sorted(self))
