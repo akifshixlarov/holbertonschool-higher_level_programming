2af127b42cdb5ddb0258683f16e7ea7b9e5b0f50
Akif <akifshixlarov@gmail.com>
2025-12-07 10:11:59 +0400

    update

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,12 +5,7 @@ import os
 
 
 def generate_invitations(template, attendees):
-    """Function that generate invitations
-
-    args:
-        template (str)
-        attendees (list[dict])
-    """
+    """Function that generates invitations"""
 
     if not template:
         print("Template must be filled")
@@ -21,35 +16,32 @@ def generate_invitations(template, attendees):
         return
 
     if not attendees:
-        print("You must fill attendees with a list of dictionnaries")
+        print("You must fill attendees with a list of dictionaries")
         return
 
     if not isinstance(attendees, list):
-        print(f"Attendees must be a list, no {type(attendees).__name__}")
+        print(f"Attendees must be a list, not {type(attendees).__name__}")
         return
 
     invalid_items = [item for item in attendees if not isinstance(item, dict)]
-    print("Item in attendees must be a dictionary")
-
     if invalid_items:
-        print(f"Items in attendees must be dictionnaries")
+        print("Item in attendees must be a dictionary")
         return
 
     for index, attendee in enumerate(attendees, start=1):
-
         invitation = template
 
         for key in ["name", "event_title", "event_date", "event_location"]:
-
+            value = attendee.get(key)
+            if not value:
+                value = "N/A"
             placeholder = "{" + key + "}"
-            value = attendee.get(key, "N/A")
             invitation = invitation.replace(placeholder, str(value))
 
-            filename = f"output_{index}.txt"
+        filename = f"output_{index}.txt"
+        if os.path.exists(filename):
+            print(f"{filename} already exists")
+            continue
 
-            if os.path.exists(filename):
-                print("{filename} already exists")
-                continue
-
-            with open(filename, "w") as file:
-                file.write(invitation)
+        with open(filename, "w") as file:
+            file.write(invitation)
